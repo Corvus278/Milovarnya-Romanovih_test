44d04e80cc6d4a1fb6820ea2f1e1e8621b40406a
Makar Korolenko <80404689+Corvus278@users.noreply.github.com>
2021-07-08 12:53:07 +0300

    Сделал позиционрование кнопок по оси Y

--- js/button-next_position.js
+++ js/button-next_position.js
@@ -1,7 +1,8 @@
 function givButtonPosition() {
   const gidrolatImg = document.querySelector('.gidrolat__img')
   const buttonSize = getComputedStyle(document.getElementsByTagName('html')[0]).getPropertyValue('--arrow-button_size')
-  const buttonMargin = getComputedStyle(document.getElementsByTagName('html')[0]).getPropertyValue('--arrow-button_margin')
+  const buttonPosition = getComputedStyle(document.getElementsByTagName('html')[0]).getPropertyValue('--arrow-button_margin')
+  const buttons = document.querySelectorAll('.gidrolat__next-button')
   const leftButton = document.querySelector('.gidrolat__next-button--left')
   const rightButton = document.querySelector('.gidrolat__next-button--right')
   const gidrolatPositionX = document.querySelector('.gidrolat-wrapper').getBoundingClientRect().left
@@ -9,17 +10,19 @@ function givButtonPosition() {
   // Позиция по оси Х
   const buttonPositionX = gidrolatPositionX - parseInt(buttonSize) - parseInt(buttonPosition)
 
-  rightButton.style.setProperty('--button-position', String(buttonPositionX))
-  leftButton.style.setProperty('--button-position', String(buttonPositionX))
+  rightButton.style.setProperty('--button_position-X', String(buttonPositionX))
+  leftButton.style.setProperty('--button_position-X', String(buttonPositionX))
 
   // Позиция по оси Y
-  const buttonPositionY = gidrolatImg.getBoundingClientRect().top + (gidrolatImg.offsetHeight / 2)
+  const buttonPositionY = (parseInt(gidrolatImg.getBoundingClientRect().top) + window.scrollY) + ((parseInt(gidrolatImg.offsetHeight) / 2) - (parseInt(buttonSize) / 2))
+  for (const button of buttons) {
+    button.style.setProperty('--button_position-Y', String(buttonPositionY))
+  }
 }
 
 
 document.addEventListener('DOMContentLoaded', () => {
   givButtonPosition()
-  // const gidrolatWrapper = querySelector('.gidrolat-wrapper')
 
   // Изменение размера экрана
   window.addEventListener('resize', () => {
