42f3367192441be313fd04f90a6aeb9ca03ea36d
Makar Korolenko <80404689+Corvus278@users.noreply.github.com>
2021-07-08 12:11:03 +0300

    Настроил позиционирование кнопок по оси X

--- js/button-next_position.js
+++ js/button-next_position.js
@@ -1,15 +1,31 @@
 function givButtonPosition() {
   const buttonSize = getComputedStyle(document.getElementsByTagName('html')[0]).getPropertyValue('--arrow-button_size')
   const buttonMargin = getComputedStyle(document.getElementsByTagName('html')[0]).getPropertyValue('--arrow-button_margin')
-  const button = document.getElementsByClassName('gidrolat__next-button')
-  const gidrolatMarginX = document.querySelector('.gidrolat-wrapper').getBoundingClientRect().right
+  const leftButton = document.querySelector('.gidrolat__next-button--left')
+  const rightButton = document.querySelector('.gidrolat__next-button--right')
+  const gidrolatMarginX = document.querySelector('.gidrolat-wrapper').getBoundingClientRect().left
 
-  buttonPositionX = gidrolatMarginX - buttonSize - buttonMargin
+  const buttonPositionX = gidrolatMarginX - parseInt(buttonSize) - parseInt(buttonMargin)
+  console.log(`${parseInt(buttonMargin)} - отступ`)
+  console.log(` ${parseInt(buttonSize)} - размер`)
 
-  button.style.setProperty('--button-right_position', String(buttonPositionX))
+  rightButton.style.setProperty('--button-position', String(buttonPositionX))
+  leftButton.style.setProperty('--button-position', String(buttonPositionX))
+  console.log(buttonPositionX)
 }
 
 
 document.addEventListener('DOMContentLoaded', () => {
   givButtonPosition()
+  // const gidrolatWrapper = querySelector('.gidrolat-wrapper')
+
+  // Изменение размера экрана
+  window.addEventListener('resize', () => {
+    givButtonPosition()
+  })
+
+  // Изменение ориентации экрана
+  window.addEventListener("orientationchange", () => {
+    givButtonPosition()
+  })
 })
