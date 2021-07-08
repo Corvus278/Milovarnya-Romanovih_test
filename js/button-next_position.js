d18391de8d8487b96f33d4b24d1ef2c4ef906ea0
Makar Korolenko <80404689+Corvus278@users.noreply.github.com>
2021-07-08 12:22:19 +0300

    Написал js для позиционирования конопок по Y

--- js/button-next_position.js
+++ js/button-next_position.js
@@ -1,17 +1,19 @@
 function givButtonPosition() {
+  const gidrolatImg = document.querySelector('.gidrolat__img')
   const buttonSize = getComputedStyle(document.getElementsByTagName('html')[0]).getPropertyValue('--arrow-button_size')
   const buttonMargin = getComputedStyle(document.getElementsByTagName('html')[0]).getPropertyValue('--arrow-button_margin')
   const leftButton = document.querySelector('.gidrolat__next-button--left')
   const rightButton = document.querySelector('.gidrolat__next-button--right')
-  const gidrolatMarginX = document.querySelector('.gidrolat-wrapper').getBoundingClientRect().left
+  const gidrolatPositionX = document.querySelector('.gidrolat-wrapper').getBoundingClientRect().left
 
-  const buttonPositionX = gidrolatMarginX - parseInt(buttonSize) - parseInt(buttonMargin)
-  console.log(`${parseInt(buttonMargin)} - отступ`)
-  console.log(` ${parseInt(buttonSize)} - размер`)
+  // Позиция по оси Х
+  const buttonPositionX = gidrolatPositionX - parseInt(buttonSize) - parseInt(buttonPosition)
 
   rightButton.style.setProperty('--button-position', String(buttonPositionX))
   leftButton.style.setProperty('--button-position', String(buttonPositionX))
-  console.log(buttonPositionX)
+
+  // Позиция по оси Y
+  const buttonPositionY = gidrolatImg.getBoundingClientRect().top + (gidrolatImg.offsetHeight / 2)
 }
 
 
