185ec24ff39af9277df0c9b50122a41f1c8b3f8a
Makar Korolenko <80404689+Corvus278@users.noreply.github.com>
2021-07-08 09:29:47 +0300

    Сделал остлеживание скролла, внес правки

---
+++ js/button-scroll.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const gidrolat = document.querySelector(".gidrolat")
+  const button = document.querySelector('.gidrolat__next-button')
+
+  const gidrolatHeight = gidrolat.offsetHeight
+  const buttonPositionTopBasic = button.offsetTop
+  let buttonPositionTopCurrent = buttonPositionTopBasic
+  const gidrolatPositionTop = gidrolat.getBoundingClientRect().top + window.scrollY
+  const viewport = window.innerHeight
+  const viewportCenter = viewport / 2
+  const buttonPositionGlobal = gidrolatPositionTop + buttonPositionTopBasic
+
+  let lastScroll = 0
+
+  document.addEventListener('scroll', () => {
+    if (buttonPositionGlobal - window.scrollY <= viewportCenter) {
+      // button.querySelector('.arrow-button__svg').classList.add('gidrolat__next-button--fixed')
+      // button.style.top = String(button.offsetTop + (window.scrollY - lastScroll)) + 'px'
+      console.log(window.scrollY - lastScroll)
+      lastScroll = window.scrollY
+    }
+  })
+})
