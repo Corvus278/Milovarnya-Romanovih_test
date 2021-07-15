fbbe52b28a8f0ee1124118758b19c27a03321b9a
Makar Korolenko <80404689+Corvus278@users.noreply.github.com>
2021-07-15 16:47:41 +0300

    Сделал слайдер для гидролатов

---
+++ js/swiper.js
@@ -0,0 +1,32 @@
+function startSwiper() {
+  const swiper = new Swiper('.swiper-container', {
+    speed: 800,
+    spaceBetween: 50,
+
+    // Breakpoints
+    breakpoints: {
+      970: {
+        spaceBetween: 75
+      },
+      1280: {
+        spaceBetween: 100
+      }
+    }
+  })
+
+  // Кнопки для переключения слайдов
+  const buttonPrev = document.querySelector('.gidrolat__next-button--left')
+  const buttonNext = document.querySelector('.gidrolat__next-button--right')
+
+  buttonPrev.addEventListener('click', () => {
+    swiper.slidePrev()
+  })
+  buttonNext.addEventListener('click', () => {
+    swiper.slideNext()
+  })
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  startSwiper()
+})
