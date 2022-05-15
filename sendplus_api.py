3506c17f61723451787f30ef45bf35c1d5a28b0e
Makar Korolenko <80404689+Corvus278@users.noreply.github.com>
2022-05-15 11:20:45 +0300

    Refactor sendpuls api

--- sendplus_api.py
+++ sendplus_api.py
@@ -2,37 +2,35 @@ import json
 from pysendpulse.pysendpulse import PySendPulse
 
 
-def send_form_data(email, name, gidrolats, api_obj):
-    """Отправка данных в адрессную книгу которая указанна в sendPulsData.json.
-    Возвращает словарь с результатом отправки """
-    adressbook_id = read_api_info()['adressbook_id']
+class SendPulseApi(PySendPulse):
 
-    dict_of_info = [{
-            'email': email,
+    def __init__(self):
+        API_INFO_DICT = self.read_api_info()
+        USER_ID = API_INFO_DICT['id']
+        USER_SECRET = API_INFO_DICT['secret']
+        TOKEN_STORAGE = 'memcached'
+        super().__init__(USER_ID, USER_SECRET, TOKEN_STORAGE)
+
+    def send_form_data(self, data_dict):
+        """Отправка данных в адрессную книгу которая указанна в sendPulsData.json.
+        Возвращает словарь с результатом отправки """
+        adressbook_id = self.read_api_info()['adressbook_id']
+
+        dict_of_info = [{
+            'email': data_dict['email'],
             'variables': {
-                'Имя': name,
-                'Гидролаты': gidrolats
+                'Имя': data_dict['name'],
+                'Гидролаты': data_dict['gidrolats']
             }
-    }]
+        }]
 
-    response = api_obj.add_emails_to_addressbook(adressbook_id, dict_of_info)
-    return response
+        response = self.add_emails_to_addressbook(adressbook_id, dict_of_info)
+        return response
 
-
-def read_api_info():
-    with open('sendPulsData.json', 'r', encoding='utf-8') as f_json:
-        f = json.load(f_json)
-        id = f['id']
-        secret = f['secret']
-        adressbook_id = f['adressbookId']
-    return {'id': id, 'secret': secret, 'adressbook_id': adressbook_id}
-
-
-def api_int():
-    API_INFO_DICT = read_api_info()
-    REST_API_ID = API_INFO_DICT['id']
-    REST_API_SECRET = API_INFO_DICT['secret']
-    TOKEN_STORAGE = 'memcached'
-    SPApiProxy = PySendPulse(REST_API_ID, REST_API_SECRET, TOKEN_STORAGE)
-    return SPApiProxy
-
+    def read_api_info(self):
+        with open('sendPulsData.json', 'r', encoding='utf-8') as f_json:
+            f = json.load(f_json)
+            id = f['id']
+            secret = f['secret']
+            adressbook_id = f['adressbookId']
+        return {'id': id, 'secret': secret, 'adressbook_id': adressbook_id}
